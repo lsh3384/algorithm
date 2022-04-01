cb13f0eaf0680cda377a275bd3470b514dfc629d
lee <lsh3384@gmail.com>
2022-04-01 21:22:48 +0900

    7-5, 7-6 finding_parts using binary search and counting search

---
+++ search/7-5_finding_parts_binary_search.py
@@ -0,0 +1,32 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+n = int(input())
+
+array = list(map(int, input().split()))
+array.sort()
+m = int(input())
+
+x = list(map(int, input().split()))
+
+for i in x:
+    result = binary_search(array, i, 0, n - 1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+'''
+5
+8 3 7 9 2
+3
+5 7 9
+'''
