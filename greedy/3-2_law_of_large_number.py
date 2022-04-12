f8a4fa0a6d18dcd0dd14f69a605d64b328636d5b
lee <lsh3384@gmail.com>
2022-04-12 22:30:28 +0900

    3-2_law_of_large_number

---
+++ greedy/3-2_law_of_large_number.py
@@ -0,0 +1,21 @@
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+
+count = int(m/ (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first
+result += (m - count) * second
+
+print(result)
+
+'''
+5 8 3
+2 4 5 4 6
+'''
