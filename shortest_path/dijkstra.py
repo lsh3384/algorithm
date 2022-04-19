ba76cf82e84b63fb15db3be1218535c7d5dfd26d
lee <lsh3384@gmail.com>
2022-04-19 21:28:05 +0900

    update

--- shortest_path/dijkstra.py
+++ shortest_path/dijkstra.py
@@ -1,26 +1,27 @@
 import sys
-
 # sys.stdin.readline함수를 input에 대입해서 input처럼 사용할 수 있도록 만들어 놓음
 input = sys.stdin.readline
 INF = int(1e9)
 
-print("노드의개수, 간선의 개수를 띄어쓰기 기준으로 입력하세요.")
+# 노드의 개수, 간선의 개수를 입력 받기
 n, m = map(int, input().split())
 
-print("시작 노드를 입력하세요.")
+# 시작 노드 번호를 입력받기
 start = int(input())
 
-
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
 graph = [[] for i in range(n + 1)]
 
-#각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+# 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
 visited = [False] * (n + 1)
 distance = [INF] * (n + 1)
 
+# 모든 간선 정보를 입력받기
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
 
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
 def get_smallest_node():
     min_value = INF
     index = 0
@@ -31,24 +32,34 @@ def get_smallest_node():
     return index
 
 def dijkstra(start):
+    # 시작 노드에 대해서 초기화
     distance[start] = 0
     visited[start] = True
     for j in graph[start]:
         distance[j[0]] = j[1]
+
+    #시작 노드를 제외한 전체 n-1개의 노드에 대해 반복
     for i in range(n - 1):
+        #현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
         now = get_smallest_node()
         visited[now] = True
 
+        # 현재 노드와 연결된 다른 노드를 확인
         for j in graph[now]:
             cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distance[j[0]]:
                 distance[j[0]] = cost
 
+# 다익스트라 알고리즘 수행
 dijkstra(start)
 
+# 모든 노드로 가기 위한 최단 거리를 출력
 for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
     if distance[i] == INF:
         print("INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
     else:
         print(distance[i])
 
