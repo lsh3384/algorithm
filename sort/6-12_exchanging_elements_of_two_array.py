0e6fc92429b5e4a5cd7a93511c1818a43c54f080
lee <lsh3384@gmail.com>
2022-04-20 21:47:54 +0900

    6-12_exchanging_elements_of_two_array

---
+++ sort/6-12_exchanging_elements_of_two_array.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+    else:
+        break
+
+print(sum(a))
