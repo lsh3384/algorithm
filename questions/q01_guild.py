d2de981307f13d5ada85e3f9906bc1f8f46cbf42
lee <lsh3384@gmail.com>
2022-04-21 22:03:12 +0900

    q01_guild

---
+++ questions/q01_guild.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+member = []
+member = list(map(int, input().split(' ')))
+
+member.sort()
+
+print(member)
+group = 0
+count = 0
+tmp_group = 0
+
+for i in member:
+    count += 1
+    if count >= i:
+        group += 1
+        count = 0
+
+print(group)
