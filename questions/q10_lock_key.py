b0aac148de6a187cd705545dee6185535e7f7b02
lee <lsh3384@gmail.com>
2022-04-27 22:53:38 +0900

    q10_lock_key still trying..

--- questions/q10_lock_key.py
+++ questions/q10_lock_key.py
@@ -1,38 +1,80 @@
-def solution(key, lock):
-    # 키 90도 돌리기
-    n = len(key)
+def turn(key, n):
     tmp = [[0] * n for k in range(n)]
-    print(tmp)
     for i in range(n):
         for j in range(n):
             tmp[j][n-1-i] = key[i][j]
-    print(tmp)
+    return tmp
 
-    # 상하좌우 확인하기
-    # 상
-    for i in range(n):
+
+def solution(key, lock):
+    # 키 90도 돌리기
+    n = len(key)
+
+    turn_array = []
+    print(key)
+
+
+    for i in range(4):
+        turn_array.append(key)
+        key = turn(key, n)
+
+    print(turn_array)
+
+    for i in turn_array:
         for j in range(n):
-            if i != n-1:
-                tmp[i][j] = tmp[i+1][j]
-            else:
-                tmp[i][j] = 0
-    print(tmp)
-
-    # 하
-    for i in range(n):
-        for j in range(n):
-            if i < n-1:
-                if i == 0:
-                    tmp[i+1][j] = tmp[i][j]
-                    tmp[i][j] = 0
-                else:
-                    tmp[i+1][j] = tmp[i][j]
-    print(tmp)
-
-    # 좌
+            for k in range(n):
+                print(i[j][k], end=' ')
+            print()
+        print()
+        
+    for new_key in turn_array:
+        if key == new_key:
+            print('일치')
+        else:
+            print('불일치')
 
 
-    # 우
+
+    # # 상하좌우 확인하기
+    # # 상
+    # for i in range(n):
+    #     for j in range(n):
+    #         if i != n-1:
+    #             tmp[i][j] = tmp[i+1][j]
+    #         else:
+    #             tmp[i][j] = 0
+    # print(tmp)
+    #
+    # # 하
+    # for i in range(n):
+    #     for j in range(n):
+    #         if i < n-1:
+    #             if i == 0:
+    #                 tmp[i+1][j] = tmp[i][j]
+    #                 tmp[i][j] = 0
+    #             else:
+    #                 tmp[i+1][j] = tmp[i][j]
+    # print(tmp)
+    #
+    # # 우
+    # for i in range(n):
+    #     for j in range(n-1, -1, -1):
+    #         if j == 0:
+    #             tmp[i][j] = 0
+    #         else:
+    #             tmp[i][j] = tmp[i][j - 1]
+    # print(tmp)
+    #
+    # # 좌
+    # for i in range(n):
+    #     for j in range(n):
+    #         if j < n-1:
+    #             tmp[i][j] = tmp[i][j + 1]
+    #         else:
+    #             tmp[i][j] = 0
+    # print(tmp)
+
+
 
 
 
