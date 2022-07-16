b212b7150b57ee5f2592fd18567555525bd913ec
lee <lsh3384@gmail.com>
2022-07-16 20:47:33 +0900

    이것이 코딩테스트다 dynamic_programming(iterative) 연습

--- problems/test.py
+++ problems/test.py
@@ -1,27 +1,21 @@
-import heapq
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
 
-n, m = map(int, input())
-start = int(input())
-graph = [[] for i in range(n + 1)]
-distance = [INF] * (n + 1)
+def quick_sort(array, start, end):
+    if start > end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
 
-for i in range(m):
-    a, b,c = map(input().split())
-    graph[a].append((b, c))
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+
+    quick_sort(array, start, right -1)
+    quick_sort(array, right + 1, end)
