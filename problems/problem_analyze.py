508a2a5837c103c5dcd79948fab36976acb5c8e6
lee <lsh3384@gmail.com>
2022-07-24 21:00:57 +0900

    permutations practice

--- problems/problem_analyze.py
+++ problems/problem_analyze.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 from collections import defaultdict
 
-
-info_dict = defaultdict()
-
+info_dict = defaultdict(int)
 print(info_dict['what'])
+
+print(list(combinations(['A', 'B', 'C'], 3)))
---
+++ problems/python_grammar/grammar_permutations.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+
+arr = ['A', 'B', 'C']
+nPr = permutations(arr, 2)
+print(list(nPr))
+
+# 결과 : [('A', 'B'), ('A', 'C'), ('B', 'A'), ('B', 'C'), ('C', 'A'), ('C', 'B')]
