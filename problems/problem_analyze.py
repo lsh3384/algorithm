09038468ff8d9a90231139cc8b69ea95cf8f4591
lee <lsh3384@gmail.com>
2022-07-25 22:14:58 +0900

    python grammar replace

--- problems/problem_analyze.py
+++ problems/problem_analyze.py
@@ -1,7 +1,18 @@
-from itertools import combinations
-from collections import defaultdict
+def solution(n, arr1, arr2):
+    ans = []
+    for i in range(n):
+        bin_str = bin(arr1[i] | arr2[2])[2:]
+        ans.append(("0" * (n- len(bin_str)) + bin_str).replace('1', '#').replace('0', ' '))
+        print(bin_str)
+    return ans
 
-info_dict = defaultdict(int)
-print(info_dict['what'])
+n = 5
+arr1 = [9, 20, 28, 18, 11]
+arr2 = [30, 1, 21, 17, 28]
 
-print(list(combinations(['A', 'B', 'C'], 3)))
+
+n = 6
+arr1 = [46, 33, 33 ,22, 31, 50]
+arr2 = [27 ,56, 19, 14, 14, 10]
+
+print(solution(n, arr1, arr2))
---
+++ problems/python_grammar/grammar_replace.py
@@ -0,0 +1,5 @@
+example_string = "gi, gello"
+
+result_string = example_string.replace('g', 'h')
+
+print(result_string)
