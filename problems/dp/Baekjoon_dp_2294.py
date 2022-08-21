0b1f633f502669a7080ae49c80e9db166b2f1b4d
lee <lsh3384@gmail.com>
2022-08-21 23:23:23 +0900

    Baekjoon - solving dp 2294 동전2

---
+++ problems/dp/Baekjoon_dp_2294.py
@@ -0,0 +1,16 @@
+# 출처: https://pacific-ocean.tistory.com/203
+n, k = map(int, input().split())
+c = []
+dp = [0 for i in range(k + 1)]
+for i in range(n):
+    c.append(int(input()))
+for i in range(1, k + 1):
+    a = []
+    for j in c:
+        if j <= i and dp[i - j] != -1:
+            a.append(dp[i - j])
+    if not a:
+        dp[i] = -1
+    else:
+        dp[i] = min(a) + 1
+print(dp[k])
